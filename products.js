10e4d522114debf222912e2cfee4c41ebdbd4bd3
Glitch (glitch-hello-node) <none>
2022-04-23 12:58:59 +0000

    🐑🍦 Checkpoint ./products.js:44068234/2210 ./productos.txt:44068234/118 ./server.js:44068234/281

---
+++ products.js
@@ -0,0 +1,101 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(name) {
+    this.fileName = name;
+    this.countID = 0;
+    this.content = [];
+    this.init();
+  }
+
+  init() {
+    let file = this.fileName;
+    try {
+      let data = fs.readFileSync(file,'utf-8', err =>{
+          if(err){
+              console.log('Archivo vacío')
+          }else{
+            this.content = data
+            for (const element of this.content) {
+                if (element.id > this.countID) this.countID = element.id;
+              }
+              console.log('archivo cargado')
+          }
+      });
+    } catch (err) {
+      console.log("No existe el archivo: ", err);
+    }
+  }
+
+  async write() {
+    try {
+      const escribir = await fs.promises.writeFile(
+        this.fileName,
+        JSON.stringify(this.content)
+      );
+      console.log("success");
+    } catch (error) {
+      console.log("Error al escribir");
+    }
+  }
+
+  save(object) {
+    this.countID++;
+    const objAdd = {
+      title: object.title,
+      price: object.price,
+      tumbnail: object.tumbnail,
+      id: this.countID,
+    };
+    this.content.push(objAdd);
+    this.write();
+    return `El id del objeto añadido es ${this.countID}.`;
+  }
+
+  getAll() {
+    return this.content;
+  }
+
+  getById(id) {
+    let result;
+    if (this.content !== []) {
+      result = this.content.find((x) => x.id === id);
+      if (result === undefined) {
+        result = null;
+      }
+    } else {
+      result = "Archivo vacío";
+    }
+    return result;
+  }
+
+  deleteById(id) {
+    let result;
+    if (this.content !== []) {
+      let newContent = this.content.filter((x) => x.id !== id);
+      this.content = newContent;
+      this.write();
+      result = `Producto fue eliminado`;
+    } else {
+      result = `Archivo vacío o el Producto no se encontró`;
+    }
+    return result;
+  }
+
+  deleteAll() {
+    this.content = [];
+       fs.writeFileSync(
+        this.fileName,
+        JSON.stringify(this.content),
+        err => {
+            if(err){
+                console.log('Error al Borrar: ', err)
+            }else{
+                console.log("Todos los Productos han sido borrados");
+            }
+        }
+      );
+  }
+}
+
+module.exports = Contenedor;
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const Contenedor = require('./products')
+
+
+let fileName  = './productos.txt'
+
+let c1 = new Contenedor(`${__dirname,fileName}`)
 
 const PORT = 8080
 
@@ -8,8 +14,8 @@ const server = app.listen(PORT, () => {
 console.log('Servidor HTTP escuchando en el puerto ' + PORT)
 })
 
-app.get('/', (req, res) => {
-res.send('<h1 style="color:blue;">Bienvenidos al servidor express</h1>')
+app.get('/productos', (req, res) => {
+  res.send(c1.getAll())
 })
 
 let cantVisitas = 0
